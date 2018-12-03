41dbd82a38dbd157cfd8c25b6d6b8f0a69ab9d6b
Juan D Vargas <juandvargas@juans-macbook-air.local>
2018-12-02 17:44:03 -0800

    Push to remote

---
+++ SB/SBCellsCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  SBCellsCollectionViewCell.swift
+//  SB
+//
+//  Created by Juan D Vargas on 30/11/2018.
+//  Copyright © 2018 spellBound. All rights reserved.
+//
+
+import UIKit
+
+class SBCellsCollectionViewCell: UICollectionViewCell {
+    
+}
